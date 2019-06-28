bd914a0fcd51e3bece05c164103f8e06284a1c18
Shamoel Hussain <shamoel.hussain@gmail.com>
2019-06-29 01:06:03 +0400

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Shamoel Hussain on 6/29/19.
+//  Copyright © 2019 Shamoel Hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
